fada4e708f3fe6f2bb96c64e7254e3adcfa24257
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-05-02 17:34:26 +0200

    feat: implement footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { Header } from '../components/header/header';
 import { links } from './app.routes';
 import { RouterOutlet } from '@angular/router';
+import { Footer } from "../components/footer/footer";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.scss',
-  imports: [Header, RouterOutlet]
+  imports: [Header, RouterOutlet, Footer]
 })
 export class App {
   protected links = links;
