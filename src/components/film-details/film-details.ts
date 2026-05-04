8fe24da1ca04f2808a99c8e01f27cc82a853ba09
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-05-04 18:09:33 +0200

    style: change naming

--- src/components/film-details/film-details.ts
+++ src/components/film-details/film-details.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnInit, Signal } from '@angular/core';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { StoreService } from '../../services/store.service';
 import { IFilm } from '../../model/films';
-import { HumanReadableDurationPipe } from '../../pipes/human-readable-duration-pipe';
+import { HumanReadableDurationPipe } from '../../pipes/human-readable-duration.pipe';
 
 @Component({
   selector: 'app-film-details',
