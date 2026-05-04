4157aed0df4dae26333d6a883c4b8281ce5c5bfd
mikhalenkadaniil <mikhalenka.d@gmail.com>
2026-05-04 20:56:08 +0200

    feat: implement about layout

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { Breadcrumbs } from "../../components/breadcrumbs/breadcrumbs";
 
 @Component({
   selector: 'app-about',
-  imports: [],
+  imports: [Breadcrumbs],
   templateUrl: './about.html',
   styleUrl: './about.scss',
 })
